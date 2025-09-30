37d51ac7d30d87036ab3d362a326145a56f4f5c2
GigiaJ <gigiaboone@yahoo.com>
2025-09-30 02:24:47 -0500

    Add traefik

--- iac/src/main/k8s/services/traefik/service.cljs
+++ iac/src/main/k8s/services/traefik/service.cljs
@@ -1,6 +1,9 @@
 (ns k8s.services.traefik.service
   (:require
-   ["@pulumi/kubernetes" :as k8s]))
+   ["@pulumi/kubernetes" :as k8s]
+   ["@pulumi/kubernetes/yaml" :as yaml]
+   ["path" :as path]
+   [configs :refer [cfg]]))
 
 (defn set-up-traefik [provider]
   (let [helm-v3 (.. k8s -helm -v3)
@@ -8,10 +11,20 @@
                    (clj->js {:chart "traefik"
                              :fetchOpts {:repo "https://helm.traefik.io/traefik"}
                              :namespace "kube-system"
-                             :values {:deployment {:kind "DaemonSet"}
+                             :values {:deployment {:kind "DaemonSet"
+                                                   :hostNetwork true
+                                                   }
                                       :ports {:web {:hostPort 80}
                                               :websecure {:hostPort 443}}
-                                      :service {:type "ClusterIP"}
+                                      :service {:type "LoadBalancer"
+                                                :externalTrafficPolicy "Local"}
+                                      :providers {:kubernetesIngress
+                                                  {:ingressStatus
+                                                   {:endpoint
+                                                    {:publishedService
+                                                     {:name "traefik"
+                                                      :namespace "kube-system"}}}}}
+                                      :additionalArguments ["--providers.kubernetesIngress.ingressClass=traefik"]
                                       }})
                    (clj->js {:provider provider}))]
     chart))
