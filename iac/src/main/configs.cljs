242d09b3c08ed139845469ea8f6b7fe4701cfa3a
GigiaJ <gigiaboone@yahoo.com>
2025-09-30 02:27:10 -0500

    Add secrets json and resource path decl

--- iac/src/main/configs.cljs
+++ iac/src/main/configs.cljs
@@ -5,11 +5,19 @@
                                 default value)))
 
 (def cfg 
-    {
+  {
    :sshKeyName (get-env "SSH_KEY_NAME" nil)
    :sshPersonalKeyName (get-env "PERSONAL_KEY_NAME" nil)
    :privateKeySsh (.toString (js/Buffer.from (get-env "PRIVATE_KEY" nil) "base64") "utf-8")
    :hcloudToken (get-env "HCLOUD_TOKEN" nil)
    :apiToken (get-env "CLOUDFLARE_TOKEN" nil)  
+   
+   ;; Non-pulumi vals
+   :resource-path (get-env "RESOURCE_PATH" "resources")  
+
+   :secrets-json (-> (js/require "path")
+                       (.join js/__dirname ".." "dummy-secrets.json")
+                       (js/require)
+                       (js->clj :keywordize-keys true))
    }
   )
