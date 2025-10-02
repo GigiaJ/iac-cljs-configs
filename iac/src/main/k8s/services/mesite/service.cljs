b4db8af2235dad9df20372ea09ad02f366b5aee8
GigiaJ <gigiaboone@yahoo.com>
2025-10-02 03:51:22 -0500

    Write my personal site service

---
+++ iac/src/main/k8s/services/mesite/service.cljs
@@ -0,0 +1,15 @@
+(ns k8s.services.mesite.service
+  (:require
+   [utils.k8s :as k8s-utils]
+   [configs :refer [cfg]]))
+
+(defn deploy [provider vault-provider]
+
+  (k8s-utils/deploy-stack
+   :namespace :vault-secrets :deployment :service :ingress
+   {:provider provider
+    :vault-provider vault-provider
+    :app-namespace "mesite"
+    :app-name "mesite"
+    :image-port 80
+    :image (str (-> cfg :docker-repo) "/mesite:latest")}))
