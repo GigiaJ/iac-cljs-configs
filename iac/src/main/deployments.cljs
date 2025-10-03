252729b232f06d1020d241e1d8dc6c95fac6b520
GigiaJ <gigiaboone@yahoo.com>
2025-10-02 21:51:29 -0500

    Set up deployments to properly use stack-ref

--- iac/src/main/deployments.cljs
+++ iac/src/main/deployments.cljs
@@ -4,18 +4,18 @@
    ["@pulumi/vault" :as vault]
    [base :as base]
    [infra.dns :as dns]
-   [k8s.services.nextcloud.service :as nextcloud-service]))
+   [k8s.services.nextcloud.service :as nextcloud-service]
+   [k8s.services.mesite.service :as mesite-service]))
 
 
-(defn app-list [config provider kc]
-  (let [stack-ref (new pulumi/StackReference "cluster")
-        vault-provider (new vault/Provider
+(defn app-list [stack-ref config provider]
+  (let [vault-provider (new vault/Provider
                             "vault-provider"
                             (clj->js {:address (.getOutput stack-ref "vaultAddress")
-                                      :token   (.getOutput stack-ref "vaultToken")})) 
+                                      :token   (.getOutput stack-ref "vaultToken")}))
         cloudflare-result (dns/setup-dns config vault-provider)
-        nextcloud-result (nextcloud-service/deploy-nextcloud provider vault-provider)
-        ]
+        nextcloud-result (nextcloud-service/deploy provider vault-provider)
+        mesite-result (mesite-service/deploy provider vault-provider)]
     {:nextcloud nextcloud-result
      :cloudflare cloudflare-result}))
 
@@ -29,8 +29,3 @@
    (base/initialize app-list) 
    (extended-exports)
    (clj->js)))
-
-#_(defn deploy-services []
-  (let [init (base/initialize app-list)]
-    (set! (.-exports js/module)
-          (clj->js (extended-exports (get init :setup) (base.build-exports init))))))
