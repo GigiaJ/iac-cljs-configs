d024ea5a433e4e6a4289821ac0e0690c16b11601
GigiaJ <gigiaboone@yahoo.com>
2025-10-07 22:24:39 -0500

    Swap to config impl

--- iac/src/main/k8s/services/mesite/service.cljs
+++ iac/src/main/k8s/services/mesite/service.cljs
@@ -1,14 +1,8 @@
-(ns k8s.services.mesite.service
-  (:require
-   [utils.k8s :as k8s-utils]
-   [configs :refer [cfg]]))
+(ns k8s.services.mesite.service (:require [configs :refer [cfg]]))
 
-(defn deploy [provider vault-provider]
-  (k8s-utils/deploy-stack
-   :namespace :vault-secrets :deployment :service :ingress
-   {:provider provider
-    :vault-provider vault-provider
-    :app-namespace "generic"
-    :app-name "mesite"
-    :image-port 80
-    :image (str (-> cfg :docker-repo) "/mesite:latest")}))
+(def config
+  {:stack [:vault-secrets :deployment :service :ingress]
+   :image-port     80
+   :app-namespace "generic"
+   :app-name      "mesite"
+   :image    (str (-> cfg :docker-repo) "/mesite:latest")})
