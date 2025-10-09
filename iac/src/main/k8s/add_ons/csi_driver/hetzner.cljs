67b084af24e893af7ddf6a210f91b86643846ebe
GigiaJ <gigiaboone@yahoo.com>
2025-10-08 23:58:12 -0500

    shorten to modern impl w/ our dynamicz

--- iac/src/main/k8s/add_ons/csi_driver/hetzner.cljs
+++ iac/src/main/k8s/add_ons/csi_driver/hetzner.cljs
@@ -1,26 +1,16 @@
 (ns k8s.add-ons.csi-driver.hetzner 
   (:require 
-   ["@pulumi/kubernetes" :as k8s]
-   ["@pulumi/pulumi" :as pulumi]))
-
-(defn execute-fn [{:keys [provider]}]
-(let [hcloud-config (pulumi/Config. "hcloud")
-  hcloud-token  (.requireSecret hcloud-config "token")
-  csi-secret    (new (.. k8s -core -v1  -Secret)
-                 "hcloud-csi-secret"
-                 (clj->js {:metadata {:name "hcloud"
-                                      :namespace "kube-system"}
-                           :stringData {:token hcloud-token}})
-                 #js {:provider provider})]
-  csi-secret))
+   [configs :refer [cfg]]))
 
 (def config
-  {:stack [:execute :chart]
+  {:stack [:secret :chart]
    :app-namespace "kube-system"
    :app-name "hcloud-csi"
-   :helm-values-fn #(clj->js {:controller {:enabled false
-                                 :existingSecret {:name "hcloud-csi-secret"}
-                                 :node {:existingSecret {:name "hcloud-csi-secret"}}}})
    :vault-load-yaml false
-   :exec-fn execute-fn
-   :chart-repo  "https://charts.hetzner.cloud"})
+   :secret-opts {:metadata {:name "hcloud"
+                            :namespace "kube-system"}
+                 :stringData {:token  (-> cfg :hcloudToken)}}
+   :chart-opts {:fetchOpts {:repo "https://charts.hetzner.cloud"}
+                :helm-values-fn #(clj->js {:controller {:enabled false
+                                                        :existingSecret {:name "hcloud-csi-secret"}
+                                                        :node {:existingSecret {:name "hcloud-csi-secret"}}}})}})
