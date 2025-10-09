8c99cf0738f8b129211791de53f4582985bdf0e7
GigiaJ <gigiaboone@yahoo.com>
2025-10-08 23:56:59 -0500

    swap to our more dynamic map passing

--- iac/src/main/k8s/add_ons/ingress_controller/caddy.cljs
+++ iac/src/main/k8s/add_ons/ingress_controller/caddy.cljs
@@ -1,20 +1,20 @@
 (ns k8s.add-ons.ingress-controller.caddy (:require [configs :refer [cfg]]))
 
 (def config
-  {:stack [:docker-image :vault-secrets :helm-fn :chart]
+  {:stack [:docker-image :chart]
    :app-namespace "caddy-system"
    :app-name      "caddy-ingress-controller"
-   :chart-repo    "https://caddyserver.github.io/ingress"
    :image-port 8080
    :vault-load-yaml false
-   :helm-values-fn #(clj->js
-                     {:ingressController
-                      {:deployment {:kind "DaemonSet"}
-                       :daemonSet {:useHostPort true}
-                       :ports {:web {:hostPort 80}
-                               :websecure {:hostPort 443}}
-                       :service {:type "NodePort"
-                                 :externalTrafficPolicy "Local"}
-                       :image {:repository (-> cfg :docker-repo)
-                               :tag "latest"}
-                       :config {:email (-> cfg :dns-email)}}})})
+   :chart-opts {:fetchOpts {:repo "https://caddyserver.github.io/ingress"}
+                :helm-values-fn #(clj->js
+                                  {:ingressController
+                                   {:deployment {:kind "DaemonSet"}
+                                    :daemonSet {:useHostPort true}
+                                    :ports {:web {:hostPort 80}
+                                            :websecure {:hostPort 443}}
+                                    :service {:type "NodePort"
+                                              :externalTrafficPolicy "Local"}
+                                    :image {:repository (-> cfg :docker-repo)
+                                            :tag "latest"}
+                                    :config {:email (-> cfg :dns-email)}}})}})
