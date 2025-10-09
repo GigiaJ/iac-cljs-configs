f97574e0aa2d8b1036ffddc709f6916e4ac93267
GigiaJ <gigiaboone@yahoo.com>
2025-10-08 23:57:07 -0500

    swap to our more dynamic map passing

--- iac/src/main/k8s/services/nextcloud/service.cljs
+++ iac/src/main/k8s/services/nextcloud/service.cljs
@@ -5,17 +5,17 @@
   {:stack [:vault-secrets :helm-fn :chart :ingress]
    :app-namespace "nextcloud"
    :app-name      "nextcloud"
-   :chart-repo    "https://nextcloud.github.io/helm/"
    :image-port 8080
    :vault-load-yaml true
-   :helm-values-fn (make-transformer
-                    (fn [{:keys [app-name secrets]}]
-                      (let [{:keys [host]} secrets]
-                        [[["nextcloud" "host"] host]
-                         [["nextcloud" "trustedDomains"] [host app-name]]])))
-   :transformations (fn [args _opts]
-                      (let [kind (get-in args [:resource :kind])]
-                        (if (some #{kind} ["StatefulSet" "PersistentVolumeClaim" "Ingress"])
-                          (update-in args [:resource :metadata :annotations]
-                                     #(assoc (or % {}) "pulumi.com/skipAwait" "true"))
-                          args)))})
+   :chart-opts {:fetchOpts {:repo "https://nextcloud.github.io/helm/"}
+                :helm-values-fn (make-transformer
+                                 (fn [{:keys [app-name secrets]}]
+                                   (let [{:keys [host]} secrets]
+                                     [[["nextcloud" "host"] host]
+                                      [["nextcloud" "trustedDomains"] [host app-name]]])))
+                :transformations (fn [args _opts]
+                                   (let [kind (get-in args [:resource :kind])]
+                                     (if (some #{kind} ["StatefulSet" "PersistentVolumeClaim" "Ingress"])
+                                       (update-in args [:resource :metadata :annotations]
+                                                  #(assoc (or % {}) "pulumi.com/skipAwait" "true"))
+                                       args)))}})
