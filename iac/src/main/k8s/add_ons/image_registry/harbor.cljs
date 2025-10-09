090260e5ec676efd362033a884ed166e96eda333
GigiaJ <gigiaboone@yahoo.com>
2025-10-08 23:57:17 -0500

    swap to our more dynamic map passing and fix values

--- iac/src/main/k8s/add_ons/image_registry/harbor.cljs
+++ iac/src/main/k8s/add_ons/image_registry/harbor.cljs
@@ -2,25 +2,25 @@
   (:require [utils.k8s :refer [make-transformer]]))
 
 (def config
-  {:stack [:namespace :vault-secrets :chart :ingress]
+  {:stack [:vault-secrets :chart :ingress]
    :app-namespace "harbor"
    :app-name "harbor"
-   :image-port 8080
-   :vault-load-yaml true
-   :chart-repo "https://helm.goharbor.io"
-   :helm-values-fn (make-transformer
-                    (fn [{:keys [app-name secrets]}]
-                      (let [{:keys [host secret-key admin-password db-password]} secrets]
-                        [[["externalURL"] (str "https://" host)]
-                         [["expose" "ingress" "enabled"] false]
-                         [["expose" "tls" "enabled"] false]
-                         [["harborAdminPassword"] admin-password]
-                         [["secretKey"] secret-key]
-                         [["database" "enabled"] true]
-                         [["database" "internal" "password"] db-password]
-                         [["persistence" "enabled"] true]
-                         [["persistence" "resourcePolicy"] "keep"]
-                         [["persistence" "persistentVolumeClaim" "registry" "storageClass"] "hetzner-csi"]
-                         [["persistence" "persistentVolumeClaim" "database" "storageClass"] "hetzner-csi"]
-                         [["persistence" "persistentVolumeClaim" "jobservice" "storageClass"] "hetzner-csi"]
-                         [["persistence" "persistentVolumeClaim" "redis" "storageClass"] "hetzner-csi"]])))})
+   :image-port 80
+   :vault-load-yaml false
+   :chart-opts {:fetchOpts {:repo "https://helm.goharbor.io"}
+                :helm-values-fn (make-transformer
+                                 (fn [{:keys [app-name secrets]}]
+                                   (let [{:keys [host secret-key admin-password db-password]} secrets]
+                                     [[["externalURL"] (str "https://" host)]
+                                      [["expose" "ingress" "enabled"] false]
+                                      [["expose" "tls" "enabled"] false]
+                                      [["harborAdminPassword"] admin-password]
+                                      [["secretKey"] secret-key]
+                                      [["database" "enabled"] true]
+                                      [["database" "internal" "password"] db-password]
+                                      [["persistence" "enabled"] true]
+                                      [["persistence" "resourcePolicy"] "keep"]
+                                      [["persistence" "persistentVolumeClaim" "registry" "storageClass"] "wasabi-csi"]
+                                      [["persistence" "persistentVolumeClaim" "database" "storageClass"] "wasabi-csi"]
+                                      [["persistence" "persistentVolumeClaim" "jobservice" "storageClass"] "wasabi-csi"]
+                                      [["persistence" "persistentVolumeClaim" "redis" "storageClass"] "wasabi-csi"]])))}})
