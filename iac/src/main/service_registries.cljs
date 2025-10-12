20b8c79f7049409c9798d82dc62d03de61bb05b3
GigiaJ <gigiaboone@yahoo.com>
2025-10-12 12:51:55 -0500

    Testing some configs

--- iac/src/main/service_registries.cljs
+++ iac/src/main/service_registries.cljs
@@ -5,13 +5,19 @@
                [k8s.add-ons.ingress-controller.caddy :as caddy]
                [k8s.add-ons.csi-driver.wasabi :as wasabi-csi]
                [k8s.add-ons.image-registry.harbor :as harbor]
+               [k8s.add-ons.minio :as minio]
+               [k8s.add-ons.s3proxy :as s3proxy]
                [k8s.services.nextcloud.service :as nextcloud-service]
                [k8s.services.mesite.service :as mesite-service]
                [k8s.services.productive.service :as productive-service]))
 
 
-(def base-service-registry [hetzner-csi/config wasabi-csi/config openbao/config])
+(def current-testing-registry [wasabi-csi/config s3proxy/config harbor/config])
 
-(def shared-service-registry [caddy/config dns/config harbor/config])
+(def base-service-registry [hetzner-csi/config openbao/config ])
+
+(def shared-service-registry [#_minio/config dns/config caddy/config 
+                              wasabi-csi/config s3proxy/config harbor/config
+                              ])
 
 (def deployment-service-registry [nextcloud-service/config mesite-service/config productive-service/config])
