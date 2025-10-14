7a13836b340f067807bfb34e6ec6fae467853bd5
GigiaJ <gigiaboone@yahoo.com>
2025-10-14 04:59:17 -0500

    Swap to our template design

--- iac/src/main/k8s/add_ons/ingress_controller/caddy.cljs
+++ iac/src/main/k8s/add_ons/ingress_controller/caddy.cljs
@@ -7,14 +7,14 @@
    :image-port 8080
    :vault-load-yaml false
    :chart-opts {:fetchOpts {:repo "https://caddyserver.github.io/ingress"}
-                :helm-values-fn #(clj->js
-                                  {:ingressController
-                                   {:deployment {:kind "DaemonSet"}
-                                    :daemonSet {:useHostPort true}
-                                    :ports {:web {:hostPort 80}
-                                            :websecure {:hostPort 443}}
-                                    :service {:type "NodePort"
-                                              :externalTrafficPolicy "Local"}
-                                    :image {:repository (-> cfg :docker-repo)
-                                            :tag "latest"}
-                                    :config {:email (-> cfg :dns-email)}}})}})
+                :value
+                {:ingressController
+                 {:deployment {:kind "DaemonSet"}
+                  :daemonSet {:useHostPort true}
+                  :ports {:web {:hostPort 80}
+                          :websecure {:hostPort 443}}
+                  :service {:type "NodePort"
+                            :externalTrafficPolicy "Local"}
+                  :image {:repository (-> cfg :docker-repo)
+                          :tag "latest"}
+                  :config {:email (-> cfg :dns-email)}}}}})
