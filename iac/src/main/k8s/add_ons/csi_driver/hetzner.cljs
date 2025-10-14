b375df23095264e7fd80618213beb749187d7d13
GigiaJ <gigiaboone@yahoo.com>
2025-10-14 05:00:01 -0500

    Swap to our template design

--- iac/src/main/k8s/add_ons/csi_driver/hetzner.cljs
+++ iac/src/main/k8s/add_ons/csi_driver/hetzner.cljs
@@ -11,6 +11,6 @@
                             :namespace "kube-system"}
                  :stringData {:token  (-> cfg :hcloudToken)}}
    :chart-opts {:fetchOpts {:repo "https://charts.hetzner.cloud"}
-                :helm-values-fn #(clj->js {:controller {:enabled false
-                                                        :existingSecret {:name "hcloud-csi-secret"}
-                                                        :node {:existingSecret {:name "hcloud-csi-secret"}}}})}})
+                :values {:controller {:enabled false
+                                      :existingSecret {:name "hcloud-csi-secret"}
+                                      :node {:existingSecret {:name "hcloud-csi-secret"}}}}}})
