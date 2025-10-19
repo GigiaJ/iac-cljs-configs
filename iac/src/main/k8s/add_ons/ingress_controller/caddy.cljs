ee88e9317f4fbb7de416189fb4cf74126958cb04
GigiaJ <gigiaboone@yahoo.com>
2025-10-19 01:46:58 -0500

    Fix caddy ingress controller

--- iac/src/main/k8s/add_ons/ingress_controller/caddy.cljs
+++ iac/src/main/k8s/add_ons/ingress_controller/caddy.cljs
@@ -1,13 +1,14 @@
 (ns k8s.add-ons.ingress-controller.caddy (:require [configs :refer [cfg]]))
 
 (def config
-  {:stack [:docker-image :chart]
+  {:stack [:docker-image :vault-secrets :chart]
    :app-namespace "caddy-system"
    :app-name      "caddy-ingress-controller"
    :image-port 8080
    :vault-load-yaml false
+   :image-opts {:imageName '(str repo "/" app-name ":latest")}
    :chart-opts {:fetchOpts {:repo "https://caddyserver.github.io/ingress"}
-                :value
+                :values
                 {:ingressController
                  {:deployment {:kind "DaemonSet"}
                   :daemonSet {:useHostPort true}
@@ -15,6 +16,6 @@
                           :websecure {:hostPort 443}}
                   :service {:type "NodePort"
                             :externalTrafficPolicy "Local"}
-                  :image {:repository (-> cfg :docker-repo)
+                  :image {:repository 'repo
                           :tag "latest"}
-                  :config {:email (-> cfg :dns-email)}}}}})
+                  :config {:email 'email}}}}})
