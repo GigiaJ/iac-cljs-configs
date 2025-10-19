a8f60d8ec9365fae4dc983ec26f7f8adc046ece6
GigiaJ <gigiaboone@yahoo.com>
2025-10-19 01:45:38 -0500

    Add secret replicator and proxy

--- iac/src/main/service_registries.cljs
+++ iac/src/main/service_registries.cljs
@@ -2,19 +2,25 @@
   (:require    [infra.openbao :as openbao]
                [k8s.add-ons.csi-driver.hetzner :as hetzner-csi]
                [infra.dns :as dns]
+               [k8s.preparers.harbor :as harbor-prepare]
                [k8s.add-ons.ingress-controller.caddy :as caddy]
                [k8s.add-ons.csi-driver.wasabi :as wasabi-csi]
                [k8s.add-ons.image-registry.harbor :as harbor]
+               [k8s.add-ons.secret-replicator :as secret-replicator]
                [k8s.add-ons.minio :as minio]
                [k8s.add-ons.s3proxy :as s3proxy]
+               [k8s.add-ons.proxy :as proxy]
                [k8s.services.nextcloud.service :as nextcloud-service]
                [k8s.services.mesite.service :as mesite-service]
+               [k8s.services.foundryvtt.service :as foundryvtt-service]
                [k8s.services.productive.service :as productive-service]))
 
 (def base-service-registry [hetzner-csi/config openbao/config ])
 
 (def shared-service-registry [#_minio/config dns/config caddy/config 
-                              wasabi-csi/config s3proxy/config harbor/config
-                              ])
+                              wasabi-csi/config proxy/config s3proxy/config harbor/config
+                              secret-replicator/config])
 
-(def deployment-service-registry [nextcloud-service/config mesite-service/config productive-service/config])
+(def prepare-service-registry [harbor-prepare/config])
+
+(def deployment-service-registry [#_nextcloud-service/config #_foundryvtt-service/config mesite-service/config productive-service/config])
