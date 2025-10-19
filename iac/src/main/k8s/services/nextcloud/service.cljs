0b3e6ff2bda3a0ce058a0fed80b43e5ebab60feb
GigiaJ <gigiaboone@yahoo.com>
2025-10-19 01:45:53 -0500

    Update Nextcloud to current format

--- iac/src/main/k8s/services/nextcloud/service.cljs
+++ iac/src/main/k8s/services/nextcloud/service.cljs
@@ -1,5 +1,4 @@
-(ns k8s.services.nextcloud.service
-  (:require [utils.k8s :refer [make-transformer]]))
+(ns k8s.services.nextcloud.service)
 
 (def config
   {:stack [:vault-secrets :chart :ingress]
@@ -8,11 +7,8 @@
    :image-port 8080
    :vault-load-yaml true
    :chart-opts {:fetchOpts {:repo "https://nextcloud.github.io/helm/"}
-                :helm-values-fn (make-transformer
-                                 (fn [{:keys [app-name secrets]}]
-                                   (let [{:keys [host]} secrets]
-                                     [[["nextcloud" "host"] host]
-                                      [["nextcloud" "trustedDomains"] [host app-name]]])))
+                :values {:nextcloud {:host 'host
+                                     :trustedDomains ['host 'app-name]}}
                 :transformations (fn [args _opts]
                                    (let [kind (get-in args [:resource :kind])]
                                      (if (some #{kind} ["StatefulSet" "PersistentVolumeClaim" "Ingress"])
