8f3c0f3a767c5572e5f59e86e5e0e1efe9511134
GigiaJ <gigiaboone@yahoo.com>
2025-10-20 21:45:15 -0500

    Add needed build args and imagePullSecrets

--- iac/src/main/k8s/services/foundryvtt/service.cljs
+++ iac/src/main/k8s/services/foundryvtt/service.cljs
@@ -1,9 +1,12 @@
 (ns k8s.services.foundryvtt.service)
 
 (def config
-  {:stack [:docker-image :vault-secrets :deployment :service :ingress]
+  {:stack [:vault-secrets :docker-image :deployment :service :ingress]
    :image-port     80
    :app-namespace "generic"
    :app-name      "foundry"
-   :image-opts {:imageName '(str repo "/" app-name ":latest")}
-   :deployment-opts {:spec {:template {:spec {:containers [{:name 'app-name :image '(str repo "/" app-name ":latest")}]}}}}})
+   :image-opts {:build {:args {:FOUNDRY_USERNAME 'FOUNDRY_USERNAME
+                               :FOUNDRY_PASSWORD 'FOUNDRY_PASSWORD}}
+                :imageName '(str repo "/" app-name ":latest")}
+   :deployment-opts {:spec {:template {:spec {:imagePullSecrets [{:name "harbor-creds-secrets"}]
+                                              :containers [{:name 'app-name :image '(str repo "/" app-name ":latest")}]}}}}})
