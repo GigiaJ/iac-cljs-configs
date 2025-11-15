ad572c04a2c6b46b0ae84bd68d7501a085c35a44
GigiaJ <gigiaboone@yahoo.com>
2025-11-14 21:39:02 -0600

    Swap to the modern config style

--- iac/src/main/k8s/add_ons/ingress_controller/caddy.cljs
+++ iac/src/main/k8s/add_ons/ingress_controller/caddy.cljs
@@ -1,13 +1,13 @@
-(ns k8s.add-ons.ingress-controller.caddy (:require [configs :refer [cfg]]))
+(ns k8s.add-ons.ingress-controller.caddy)
 
 (def config
-  {:stack [:docker-image :vault-secrets :chart]
+  {:stack [:docker:docker-image :k8s:vault-secrets :k8s:chart]
    :app-namespace "caddy-system"
    :app-name      "caddy-ingress-controller"
-   :image-port 8080
-   :vault-load-yaml false
-   :image-opts {:imageName '(str repo "/" app-name ":latest")}
-   :chart-opts {:fetchOpts {:repo "https://caddyserver.github.io/ingress"}
+   :k8s:image-port 8080
+   :k8s:vault-load-yaml false
+   :k8s:image-opts {:imageName '(str repo "/" app-name ":latest")}
+   :k8s:chart-opts {:fetchOpts {:repo "https://caddyserver.github.io/ingress"}
                 :values
                 {:ingressController
                  {:deployment {:kind "DaemonSet"}
