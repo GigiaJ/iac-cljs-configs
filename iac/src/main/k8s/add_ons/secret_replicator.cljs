557bfeb7fecc9f2b84f07e1edf4eefce351faaab
GigiaJ <gigiaboone@yahoo.com>
2025-11-16 13:10:10 -0600

    Update to modern config style

--- iac/src/main/k8s/add_ons/secret_replicator.cljs
+++ iac/src/main/k8s/add_ons/secret_replicator.cljs
@@ -1,9 +1,9 @@
 (ns k8s.add-ons.secret-replicator)
 
 (def config
-  {:stack [:chart]
+  {:stack [:k8s:chart]
    :image-port     80
    :no-namespace true
    :app-namespace "kube-system"
    :app-name      "kubernetes-replicator"
-   :chart-opts {:fetchOpts {:repo "https://helm.mittwald.de"}}})
+   :k8s:chart-opts {:fetchOpts {:repo "https://helm.mittwald.de"}}})
