69a147d685a935a0e6790a859a59cf0bf6946c3e
GigiaJ <gigiaboone@yahoo.com>
2025-11-16 13:10:37 -0600

    Update to use new docker-build and points to the set cfgs for the public registry we store the Docker image

--- iac/src/main/k8s/add_ons/ingress_controller/caddy.cljs
+++ iac/src/main/k8s/add_ons/ingress_controller/caddy.cljs
@@ -1,21 +1,27 @@
-(ns k8s.add-ons.ingress-controller.caddy)
+(ns k8s.add-ons.ingress-controller.caddy
+  (:require [configs :refer [cfg]]))
 
 (def config
-  {:stack [:docker:docker-image :k8s:vault-secrets :k8s:chart]
+  {:stack [:vault:prepare :docker:image :k8s:secret :k8s:chart]
    :app-namespace "caddy-system"
    :app-name      "caddy-ingress-controller"
    :k8s:image-port 8080
    :k8s:vault-load-yaml false
    :k8s:image-opts {:imageName '(str repo "/" app-name ":latest")}
+   :docker:image-opts {:registry {:server (-> cfg :public-image-registry-url)
+                                  :username (-> cfg :public-image-registry-username)
+                                  :password (-> cfg :public-image-registry-password)}
+                       :tags [(str (-> cfg :public-image-registry-url) "/" (-> cfg :public-image-registry-username) "/" "caddy")]
+                       :push true}
    :k8s:chart-opts {:fetchOpts {:repo "https://caddyserver.github.io/ingress"}
-                :values
-                {:ingressController
-                 {:deployment {:kind "DaemonSet"}
-                  :daemonSet {:useHostPort true}
-                  :ports {:web {:hostPort 80}
-                          :websecure {:hostPort 443}}
-                  :service {:type "NodePort"
-                            :externalTrafficPolicy "Local"}
-                  :image {:repository 'repo
-                          :tag "latest"}
-                  :config {:email 'email}}}}})
+                    :values
+                    {:ingressController
+                     {:deployment {:kind "DaemonSet"}
+                      :daemonSet {:useHostPort true}
+                      :ports {:web {:hostPort 80}
+                              :websecure {:hostPort 443}}
+                      :service {:type "NodePort"
+                                :externalTrafficPolicy "Local"}
+                      :image {:repository 'repo
+                              :tag "latest"}
+                      :config {:email 'email}}}}})
