9833ddb7b86df0d6e059afa4969af91418ccc3bd
GigiaJ <gigiaboone@yahoo.com>
2025-11-22 17:04:14 -0600

    Update to use HTTPRoute and Gateway API

--- iac/src/main/k8s/services/mesite/service.cljs
+++ iac/src/main/k8s/services/mesite/service.cljs
@@ -1,20 +1,34 @@
 (ns k8s.services.mesite.service)
 
+(defn test [env]
+  (js/console.log env)
+  (.apply (:test env) #(js/console.log %)))
+
 (def config
   {:stack [:vault:prepare
            :harbor:robot-account
            :docker:image
-           [:k8s :namespace :deployment :service :ingress]]
+           [:k8s :namespace :deployment :service :ingress :httproute]] 
    :app-name "mesite"
+   :namespace "generic"
    :docker:image-opts {:context {:location "https://codeberg.org/Gigia/mesite.git"}
-                       :registry {:server 'repo
+                       :imageName '(str registry-base "/" registry-namespace "/" app-name ":latest")
+                       :registry {:server '(str registry-base "/" registry-namespace)
                                   :username '(-> :harbor:robot-account .-name)
                                   :password '(-> :harbor:robot-account .-secret)}
                        :tags ['(str registry-base "/" registry-namespace "/" app-name)]
                        :push true}
 
-   :harbor:robot-account-opts {:permissions [{:kind "project"
+   :harbor:robot-account-opts {:name 'app-name
+                               :permissions [{:kind "project"
                                               :namespace 'registry-namespace
-                                              :access [{:action "push" :resource "repository"}
-                                                       {:action "list" :resource "repository"}]}]}
-   :k8s:deployment-opts {:spec {:template {:spec {:imagePullSecrets [{:name "harbor-creds-secrets"}]}}}}})
+                                              :access [{:action "pull" :resource "repository"}
+                                                       {:action "push" :resource "repository"}
+                                                       {:action "list" :resource "repository"}]}]} 
+   
+   :k8s:deployment-opts {:spec {:template {:spec {:imagePullSecrets [{:name "harbor-creds-secrets"}]
+                                                  :containers [{:name 'app-name
+                                                                :image '(str registry-base "/" registry-namespace "/" app-name ":latest")
+                                                                :ports [{:containerPort 80}]}]}}}} 
+   :k8s:httproute-opts {:spec {::hostnames ['host]}}
+   })
