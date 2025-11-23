788e72b3c9bce0645b88c5766247648dbd7632b7
GigiaJ <gigiaboone@yahoo.com>
2025-11-23 15:34:45 -0600

    Update to use docker-build format and modern config style

--- iac/src/main/k8s/services/foundryvtt/service.cljs
+++ iac/src/main/k8s/services/foundryvtt/service.cljs
@@ -1,12 +1,24 @@
 (ns k8s.services.foundryvtt.service)
 
 (def config
-  {:stack [:vault-secrets :docker-image :deployment :service :ingress]
+  {:stack [:vault:prepare :harbor:robot-account :docker:image [:k8s :deployment :service :httproute]]
    :image-port     30000
    :app-namespace "generic"
    :app-name      "foundry"
-   :image-opts {:build {:args {:FOUNDRY_USERNAME 'FOUNDRY_USERNAME
-                               :FOUNDRY_PASSWORD 'FOUNDRY_PASSWORD}}
-                :imageName '(str repo "/" app-name ":latest")}
-   :deployment-opts {:spec {:template {:spec {:imagePullSecrets [{:name "harbor-creds-secrets"}]
-                                              :containers [{:name 'app-name :image '(str repo "/" app-name ":latest")}]}}}}})
+   :docker:image-opts {:is-local true
+                       :buildArgs {:FOUNDRY_USERNAME 'FOUNDRY_USERNAME
+                                   :FOUNDRY_PASSWORD 'FOUNDRY_PASSWORD} 
+                       :registry {:server '(str registry-base "/" registry-namespace)
+                                  :username '(-> :harbor:robot-account .-name)
+                                  :password '(-> :harbor:robot-account .-secret)}
+                       :tags ['(str registry-base "/" registry-namespace "/" app-name)]
+                       :push true}
+   :k8s:deployment-opts {:spec {:template {:spec {:imagePullSecrets [{:name "harbor-creds-secrets"}]
+                                                  :containers [{:name 'app-name :image '(str registry-base "/" registry-namespace "/" app-name ":latest")}]}}}}
+   :harbor:robot-account-opts {:name 'app-name
+                               :permissions [{:kind "project"
+                                              :namespace 'registry-namespace
+                                              :access [{:action "pull" :resource "repository"}
+                                                       {:action "push" :resource "repository"}
+                                                       {:action "list" :resource "repository"}]}]}
+   :k8s:httproute-opts {:spec {::hostnames ['host]}}})
