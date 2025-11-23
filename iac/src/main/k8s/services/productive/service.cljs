42b6716850d9311dbd55285fad4c736243c02203
GigiaJ <gigiaboone@yahoo.com>
2025-11-23 15:35:13 -0600

    Update to use the current config style

--- iac/src/main/k8s/services/productive/service.cljs
+++ iac/src/main/k8s/services/productive/service.cljs
@@ -1,8 +1,10 @@
 (ns k8s.services.productive.service)
 
 (def config
-  {:stack [:vault-secrets :deployment :service :ingress]
+  {:stack [:vault:prepare [:k8s :deployment :service :httproute]]
    :app-namespace "generic"
    :app-name      "superproductivity"
    :image-port     80
-   :image   "docker.io/johannesjo/super-productivity:latest"})
+   :image   "docker.io/johannesjo/super-productivity:latest"
+   :k8s:httproute-opts {:spec {::hostnames ['host]}}
+   })
