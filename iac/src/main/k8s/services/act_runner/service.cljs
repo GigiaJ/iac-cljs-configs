33f837a5f772ade74d6695721456c5a1ff804887
GigiaJ <gigiaboone@yahoo.com>
2025-11-23 15:35:08 -0600

    Update to use the current config style

--- iac/src/main/k8s/services/act_runner/service.cljs
+++ iac/src/main/k8s/services/act_runner/service.cljs
@@ -2,12 +2,13 @@
 
 
 (def config
-  {:stack [:vault-secrets :deployment :service]
+  {:stack [:vault:prepare [:k8s :deployment :service]]
    :image-port     80
    :app-namespace "generic"
    :app-name      "act-runner"
-   :deployment-opts {:spec {:template {:spec {:containers [{:name 'app-name
+   :k8s:deployment-opts {:spec {:template {:spec {:containers [{:name 'app-name
                                                             :envFrom [{:secretRef {:name '(str app-name "-secrets")}}]
                                                             :image '(str repo "/" "act_runner" ":latest")
-                                                            }]
-                                              }}}}})
+                                                            }]}}}}
+   :k8s:httproute-opts {:spec {::hostnames ['host]}}
+   })
