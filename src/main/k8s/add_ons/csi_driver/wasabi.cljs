8dd5e7aa3c34ce353ca6c9f452a3ed5d4b470d86
GigiaJ <gigiaboone@yahoo.com>
2025-11-29 04:04:24 -0600

    Simplify wasabi-csi

--- src/main/k8s/add_ons/csi_driver/wasabi.cljs
+++ src/main/k8s/add_ons/csi_driver/wasabi.cljs
@@ -10,17 +10,7 @@
                 :repositoryOpts {:repo "https://yandex-cloud.github.io/k8s-csi-s3/charts"}
                 :values {:controller {:enabled false
                                                         :existingSecret {:name "wasabi-csi-secrets"}
-                                                        :node {:existingSecret {:name "wasabi-csi-secrets"}}}}
-                                          
-                                          #_:storageClass #_{:create true
-                                                         :name "csi-s3-sc"
-                                                         :singleBucket "pulumi-harbor"
-                                                         :region "us-east-1"
-                                                         :accessKeyID "something"
-                                                         :secretAccessKey "something"
-                                                         ;;:bucket "pulumi-harbor"
-                                                         }}
+                                                        :node {:existingSecret {:name "wasabi-csi-secrets"}}}}}
    :k8s:secret-opts {:stringData {:accessKeyID (-> cfg :wasabiId)
                              :secretAccessKey (-> cfg :wasabiKey)
-                             :endpoint "http://wasabi-proxy.wasabi-proxy.svc.cluster.local"}}
-  :vault-load-yaml false})
+                             :endpoint "http://wasabi-proxy.wasabi-proxy.svc.cluster.local"}}})
