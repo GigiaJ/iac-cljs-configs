01e405d125be4728579b92a580a15d4c1c234c1b
GigiaJ <gigiaboone@yahoo.com>
2025-11-30 05:19:53 -0600

    Moved livekit into element-call

--- src/main/k8s/services/matrix/livekit_jwt/service.cljs
+++
@@ -1,8 +0,0 @@
-(ns k8s.services.matrix.livekit-jwt.service)
-
-(def config
-  {:stack [:vault-secrets :docker-image :deployment :service :ingress]
-   :image-port     80
-   :app-namespace "matrix"
-   :app-name      "livekit-jwt"
-   :deployment-opts {:spec {:template {:spec {:containers [{:name 'app-name :image '(str repo "/" lk-jwt-service ":0.2.3")}]}}}}})
