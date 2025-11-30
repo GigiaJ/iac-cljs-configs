97ebd04d9967fd5852c9754a75ada111044b16c2
GigiaJ <gigiaboone@yahoo.com>
2025-11-30 05:22:46 -0600

    Fix Nextcloud (wtf broadcom)

--- src/main/k8s/services/nextcloud/service.cljs
+++ src/main/k8s/services/nextcloud/service.cljs
@@ -2,19 +2,38 @@
 
 ;; Need to automate set-up/restore
 (def config
-  {:stack [:vault-secrets :k8s:chart :k8s:httproute]
+  {:stack [:vault:prepare :k8s:chart :k8s:httproute]
    :app-namespace "nextcloud"
    :app-name      "nextcloud"
-   :image-port 8080
+   :image-port 80
    :vault-load-yaml true
    :k8s:chart-opts {:repositoryOpts {:repo "https://nextcloud.github.io/helm/"}
-                :values {:nextcloud {:host 'host
-                                     :trustedDomains ['host 'app-name]}}
-                :transformations (fn [args _opts]
-                                   (let [kind (get-in args [:resource :kind])]
-                                     (if (some #{kind} ["StatefulSet" "PersistentVolumeClaim" "Ingress"])
-                                       (update-in args [:resource :metadata :annotations]
-                                                  #(assoc (or % {}) "pulumi.com/skipAwait" "true"))
-                                       args)))}
-   :k8s:httproute-opts {:spec {::hostnames ['host]}}
-   })
+                    :values
+                    {:nextcloud {:host 'host
+                                 :containerPort 80
+                                 :trustedDomains ['host 'app-name]
+                                 :persistence {:enabled true
+                                               :storageClass "juicefs-sc"
+                                               :accessMode "ReadWriteMany"
+                                               :size "1Ti"}}
+                     :service {:port 80}
+                     :mariadb {:enabled true
+                               :architecture "standalone"
+                               :primary {:persistence {:enabled true
+                                                       :storageClass "hcloud-volumes"
+                                                       :size "8Gi"}}
+                               ;; Obligatory what the fuck Broadcom, why are you like this. RIP Bitnami
+                               :volumePermissions {:enabled true
+                                                   :image {:registry "docker.io"
+                                                           :repository "bitnami/os-shell"
+                                                           :tag "latest"
+                                                           :pullPolicy "Always"}}
+                               }
+
+                     :transformations (fn [args _opts]
+                                        (let [kind (get-in args [:resource :kind])]
+                                          (if (some #{kind} ["StatefulSet" "PersistentVolumeClaim" "Ingress"])
+                                            (update-in args [:resource :metadata :annotations]
+                                                       #(assoc (or % {}) "pulumi.com/skipAwait" "true"))
+                                            args)))}}
+   :k8s:httproute-opts {:spec {::hostnames ['host]}}})
