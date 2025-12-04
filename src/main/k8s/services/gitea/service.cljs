cff89723668a861e2d6b4609e8a771c6b544d4aa
GigiaJ <gigiaboone@yahoo.com>
2025-12-04 03:01:19 -0600

    Fix Gitea c:

--- src/main/k8s/services/gitea/service.cljs
+++ src/main/k8s/services/gitea/service.cljs
@@ -1,54 +1,51 @@
 (ns k8s.services.gitea.service)
 
 (def config
-  {:stack [:vault:prepare :k8s:pvc :k8s:deployment :k8s:service :k8s:httproute]
-   :image-port    3000
+  {:stack [:vault:prepare :k8s:pvc :k8s:deployment :k8s:service :k8s:httproute :k8s:tcproute]
    :app-namespace "generic"
    :app-name      "gitea"
-
+   :image-port 3000
    :k8s:pvc-opts
-   {"gitea-data"   {:storageClass "juicefs-sc" :accessModes ["ReadWriteMany"] :storage "1Ti"}
-    "gitea-config" {:storageClass "juicefs-sc" :accessModes ["ReadWriteMany"] :storage "1Gi"}}
+   {:metadata {:name "gitea-state"
+               :namespace "generic"}
+    :spec {:storageClassName "juicefs-sc"
+           :accessModes ["ReadWriteMany"]
+           :resources {:requests {:storage "1Ti"}}}}
 
    :k8s:deployment-opts
    {:spec
     {:template
-     {:spec
-      {:initContainers
-       [{:name "init-permissions"
-         :image "busybox:latest"
-         :command ["sh" "-c" "chown -R 1000:1000 /var/lib/gitea && chown -R 1000:1000 /etc/gitea"]
-         :volumeMounts [{:name "gitea-data" :mountPath "/var/lib/gitea"}
-                        {:name "gitea-config" :mountPath "/etc/gitea"}]
-         :securityContext {:runAsUser 0 :runAsGroup 0}}]
-
-       :containers
+     {:metadata {:annotations {"backup.velero.io/backup-volumes" "gitea-state"}}
+      :spec
+      {:containers
        [{:name 'app-name
          :image '(str repo "/" app-name ":latest-rootless")
-
+         :command ["/usr/local/bin/gitea"]
+         :args ["web"
+                "-c" "/var/lib/gitea/custom/conf/app.ini"]
          :env [{:name "TZ" :value "America/Chicago"}]
          :envFrom [{:secretRef {:name "gitea-secrets"}}]
-         :ports [{:name "http" :containerPort 3000}
-                 {:name "ssh"  :containerPort 2222}]
+         :ports [{:name "ssh"  :containerPort 2222}]
 
-         :volumeMounts [{:name "gitea-data" :mountPath "/var/lib/gitea"}
-                        {:name "gitea-config" :mountPath "/etc/gitea"}]}]
+         :volumeMounts [{:name "gitea-state" :mountPath "/var/lib/gitea"}]}]
 
        :volumes
-       [{:name "gitea-data" :persistentVolumeClaim {:claimName "gitea-data"}}
-        {:name "gitea-config" :persistentVolumeClaim {:claimName "gitea-config"}}]}}}}
+       [{:name "gitea-state"
+         :persistentVolumeClaim {:claimName "gitea-state"}}]}}}}
 
    :k8s:service-opts
    {:spec
     {:type "NodePort"
      :selector {:app "gitea"}
-     :ports [
-             {:name 'app-name :port 3000 :targetPort 3000}
+     :ports [{:name 'app-name :port 3000 :targetPort 3000}
 
              {:name "ssh"
-              :port 22         
-              :targetPort 2222 
-              :nodePort 30022 
-              }]}}
+              :port 22
+              :targetPort 2222
+              :nodePort 30022}]}}
+   :k8s:httproute-opts {:spec {::hostnames ['host]
+                               :rules [{:matches [{:path {:type "PathPrefix"
+                                                          :value "/"}}]
+                                        :backendRefs [{:name 'app-name
+                                                       :port 3000}]}]}}})
 
-   :k8s:httproute-opts {:spec {::hostnames ['host]}}})
